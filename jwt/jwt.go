0d7ff4062a2c1bf5814862438653a5242cb03f8f
Anton Bronnikov <anton@northernforest.nl>
2025-06-07 16:27:03 +0200

    debug: log invalid jwt tokens

--- jwt/jwt.go
+++ jwt/jwt.go
@@ -20,8 +20,8 @@ type Claims struct {
 func IssuedAt(token string) (time.Time, error) {
 	parts := strings.Split(token, ".")
 	if len(parts) != 3 {
-		return time.Time{}, fmt.Errorf("%w: invalid count of parts (expect 3, got %d)",
-			errInvalidJwt, len(parts),
+		return time.Time{}, fmt.Errorf("%w: invalid count of parts (expect 3, got %d): %s",
+			errInvalidJwt, len(parts), token,
 		)
 	}
 
